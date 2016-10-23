a62ca0dba22b7cbaf64d91997e6222b4ff4c5917
roli <roli@cock.li>
2016-10-22 20:43:14 -0500

    Adding Data Access Layer.

---
+++ Store/StoreDAL/ProductDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StoreBDO;
+
+namespace StoreDAL
+{
+    public class ProductDAO
+    {
+
+        public ProductBDO GetProduct(int id)
+        {
+            // TODO: Connect to the DB
+            var p = new ProductBDO();
+            p.ProductID = id;
+            p.ProductName = "product from DAL";
+            p.UnitPrice = 10.00m;
+            p.QuantityPerUnit = "Kg.";
+
+            return p;
+        }
+
+        public bool UpdateProduct(ProductBDO product, ref string message)
+        {
+            // TODO: Connect to the DB
+            message = "product updated successfully";
+            return true;
+        }
+
+    }
+}
